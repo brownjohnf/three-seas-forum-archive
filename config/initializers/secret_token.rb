9c2ac048aef902754d025390dba5b5933d7a124c
John F. Brown <jack@brownjohnf.com>
2013-01-25 21:05:48 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThreeSeasForum::Application.config.secret_token = '6b09d8ddcd3be0e56537bac7283a84d7d3799f0a8ef248ee9b3bbd7129d3ce2d6a7cf2cbdf09118d8e5b90038ede84023451bd94701e12155a9dc5fe8e440d9f'
